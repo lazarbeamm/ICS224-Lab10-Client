4b5eaea0e841939e1b871271f22625ab709ba77d
Michael Horie <michaelhorie@shaw.ca>
2022-02-24 20:15:31 -0800

    Initial commit

---
+++ Client/ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by Michael on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Client/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Client
+//
+//  Created by Michael on 2022-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var message = ""
+    @StateObject var networkSupport = NetworkSupport(browse: true)
+    @State var outgoingMessage = ""
+    
+    var body: some View {
+        VStack {
+            if !networkSupport.connected {
+                TextField("Message", text: $message)
+                    .multilineTextAlignment(.center)
+                
+                List ($networkSupport.peers, id: \.self) {
+                    $peer in
+                    Button(peer.displayName) {
+                        do {
+                            try networkSupport.contactPeer(peerID: peer, request: Request(details: message))
+                        }
+                        catch let error {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            else {
+                TextField("Message", text: $outgoingMessage)
+                    .multilineTextAlignment(.center)
+                
+                Button("Send") {
+                    networkSupport.send(message: outgoingMessage)
+                    outgoingMessage = ""
+                }
+            }
+        }
+        .padding()
+    }
+}
